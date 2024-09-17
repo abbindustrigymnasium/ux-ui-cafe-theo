2a0fe42826a4b4a8d8f3eaa5d5a7667f2a849798
IanTerzo <ian.baldelli@gmail.com>
2024-09-17 11:37:25 +0200

    Event page improvements

---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<script setup>
+    let logo = import("../../public/icons/logo_compressed.svg")
+</script>
+<template>
+    <nav>
+        <img id="logo" src="icons/logo_compressed.svg">
+        <ul id="nav-options">
+            <li>
+                <a class="nav-link" href="/upcoming">Upcoming Events</a>
+            </li>
+            <li>
+                <a class="nav-link" href="/cakes">Cakes</a>
+            </li>
+            <li>
+                <a class="nav-link" href="/staff/">Personel</a>
+            </li>
+            <li>
+                <a class="nav-link" href="/philosophy">Our Philosophy</a>
+            </li>
+
+        </ul>
+    </nav>
+</template>
+
+<style>
+    /* @import url("src/css/quasar.variables.scss"); */
+    nav {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        /* background-color: rgb(91, 21, 21); */
+        background-color: #4F4545;
+        padding-inline: 20%;
+    }
+    #nav-options{
+        display: flex;
+        list-style: none;
+    }
+    .nav-link{
+        color: white;
+        text-decoration: none;
+        margin-left: 1em;
+    }
+
+
+</style>
